56f54299a17270dbdf23f832bc98adfce1f65b75
Hamza <Hamza5955>
2023-10-12 16:16:32 +0200

    clear

--- 1. Two Sum/twosum.php
+++ 1. Two Sum/twosum.php
@@ -8,10 +8,15 @@ class Solution {
  * @return Integer[]
  */
     function twoSum($nums, $target) {
+
         for ($i = 0; $i < count($nums); $i++) {
+
             for ($j = $i + 1; $j < count($nums); $j++) {
+
                 if ($nums[$i] + $nums[$j] == $target) {
+
                     return array($i, $j);
+
                 }
             }
         }
