91b62be62190205b507998692c8d886e2d99813e
Hamza <Hamza5955>
2023-10-17 18:09:12 +0200

    top k frequent elements

---
+++ 347. Top K Frequent Elements/topkfrequentelements.js
@@ -0,0 +1,27 @@
+
+// Source : https://leetcode.com/problems/top-k-frequent-elements/description/
+
+/*************************************************************************************************
+ * 
+ * 
+ *  Given an integer array nums and an integer k, return the k most frequent elements.
+ *  You may return the answer in any order.
+ * 
+ *  Example 1:
+ *  Input: nums = [1,1,1,2,2,3], k = 2
+ *  Output: [1,2]
+ * 
+ * Example 2:
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ * 
+ ************************************************************************************************/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    
+};
