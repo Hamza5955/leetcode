384273d43529abe830ccef7910abb4f49e3310e0
Hamza <Hamza5955>
2023-10-17 18:55:53 +0200

    done

--- 347. Top K Frequent Elements/topkfrequentelements.js
+++ 347. Top K Frequent Elements/topkfrequentelements.js
@@ -11,9 +11,9 @@
  *  Input: nums = [1,1,1,2,2,3], k = 2
  *  Output: [1,2]
  * 
- * Example 2:
- * Input: nums = [1], k = 1
- * Output: [1]
+ *  Example 2:
+ *  Input: nums = [1], k = 1
+ *  Output: [1]
  * 
  ************************************************************************************************/
 
@@ -24,4 +24,31 @@
  */
 var topKFrequent = function(nums, k) {
     
+    // initializes an empty Map
+
+    let map = new Map();
+
+    // if the number dass not exit add else increment bay one
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (!map.has(nums[i])) {
+
+            map.set(nums[i], 1);
+
+        } else {
+
+            map.set(nums[i], map.get(nums[i]) + 1);
+
+        }
+
+    }
+
+    // sort the map by value 
+
+    let sortedMap = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+
+    // retun the k ammaunt of ellent 
+    return [...sortedMap.keys()].slice(0, k);
+
 };
