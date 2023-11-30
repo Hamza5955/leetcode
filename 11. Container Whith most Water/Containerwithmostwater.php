ebd1b37262019aa1c4dd0de9daa2140d9fe5b074
Hamza5955 <jamilhamza2000@gmail.com>
2023-11-30 20:35:39 +0100

    time Complexity and Big o notation

--- 11. Container Whith most Water/Containerwithmostwater.php
+++ 11. Container Whith most Water/Containerwithmostwater.php
@@ -1,5 +1,23 @@
 <?php
 
+
+/*********************************************************************************************
+ * 
+ *  You are given an integer array height of length n. 
+ *  There are n vertical lines drawn such that the two 
+ *  endpoints of the ith line are (i, 0) and (i, height[i]).
+ *  Find two lines that together with the x-axis form a container, 
+ *  such that the container contains the most water.
+ *  Return the maximum amount of water a container can store.
+ * 
+ *  Input: height = [1,8,6,2,5,4,8,3,7]
+ *  Output: 49
+ *  Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+ *  In this case, the max area of water (blue section) the container can contain is 49.
+ * 
+ *********************************************************************************************/
+
+
 class Solution {
 
     /**
@@ -64,6 +82,15 @@ class Solution {
         return $j;
 
     }
+
+    // The Big O notation for this algorithm is as follows:
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    // Where 'n' represents the length of the 'heights'/'nums' array.
+    // The time complexity is O(n) because, in the worst-case scenario, we loop through the entire 'heights'/'nums' array exactly once.
+    // The space complexity is O(n) because, in the worst-case scenario, we store all the values of the 'heights' array in the set.
+
+
 
 }
 
